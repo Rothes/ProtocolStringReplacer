15a3b49dedfc9877b838fc4042b5bca94cc971d9
Rothes <449181985@qq.com>
2021-06-26 15:49:37 +0800

    Use json for Components.

--- src/main/java/me/Rothes/ProtocolStringReplacer/PacketListeners/Server/Chat.java
+++ src/main/java/me/Rothes/ProtocolStringReplacer/PacketListeners/Server/Chat.java
@@ -1,6 +1,6 @@
 package me.Rothes.ProtocolStringReplacer.PacketListeners.Server;
 
-import me.Rothes.ProtocolStringReplacer.API.ComponentConverter;
+import io.papermc.paper.text.PaperComponents;
 import me.Rothes.ProtocolStringReplacer.User.User;
 import me.Rothes.ProtocolStringReplacer.ProtocolStringReplacer;
 import com.comphenix.protocol.PacketType;
@@ -10,20 +10,17 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
-import org.apache.commons.lang.Validate;
-import org.bukkit.Bukkit;
+import org.apache.commons.lang3.StringEscapeUtils;
 
-import javax.annotation.Nonnull;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 public final class Chat extends AbstractServerPacketListener {
+
+    private GsonComponentSerializer paperGsonComponentSerializer;
 
     public Chat() {
         super(PacketType.Play.Server.CHAT);
@@ -43,25 +40,16 @@ public final class Chat extends AbstractServerPacketListener {
                     wrappedChatComponentStructureModifier.write(0, wrappedChatComponent);
                 } else {
                     StructureModifier<Object> structureModifier = packet.getModifier();
-                    int fieldIndex = 0;
-                    boolean foundComponent = false;
-                    Object read = null;
-                    // Paper has an extra field for its component.
-                    while (fieldIndex < 2 && !(read instanceof BaseComponent[] || isPaperComponent(read))) {
-                        read = structureModifier.read(++fieldIndex);
-                        if (read instanceof BaseComponent[] || isPaperComponent(read)) {
-                            foundComponent = true;
-                            break;
-                        }
-                    }
-
-                    if (foundComponent) {
-                        if (isPaperComponent(read)) {
-                            BaseComponent[] baseComponents = getReplacedComponents(ComponentConverter.paperToSpigot((net.kyori.adventure.text.TextComponent) read), user);
-                            Bukkit.getConsoleSender().sendMessage(Arrays.toString(baseComponents));
-                            structureModifier.write(fieldIndex, ComponentConverter.spigotToPaper(baseComponents));
-                        } else {
-                            structureModifier.write(fieldIndex, getReplacedComponents((BaseComponent[]) read, user));
+                    for (int fieldIndex = 1; fieldIndex < 3; fieldIndex++) {
+                        Object read = structureModifier.read(fieldIndex);
+                        if (read instanceof BaseComponent[]) {
+                            structureModifier.write(fieldIndex, ComponentSerializer.parse(ProtocolStringReplacer.getInstance().getReplacerManager().getReplacedString(ComponentSerializer.toString(read), user, filter)));
+                        } else if (isPaperComponent(read)) {
+                            structureModifier.write(fieldIndex, getPaperGsonComponentSerializer().deserialize(
+                                    ProtocolStringReplacer.getInstance().getReplacerManager().getReplacedString(
+                                            StringEscapeUtils.unescapeJson(getPaperGsonComponentSerializer().serialize((TextComponent) read)), user, filter
+                                    )
+                            ));
                         }
                     }
                 }
@@ -69,50 +57,15 @@ public final class Chat extends AbstractServerPacketListener {
         }
     };
 
-    private boolean hasExtra(@Nonnull BaseComponent baseComponent) {
-        return baseComponent.getExtra() != null;
-    }
-
-    @Nonnull
-    private BaseComponent[] getReplacedComponents(@Nonnull BaseComponent[] baseComponents, @Nonnull User user) {
-        for (int i = 0; i < baseComponents.length; i++) {
-            BaseComponent baseComponent = baseComponents[i];
-            baseComponent = getReplacedComponent(baseComponent, user);
-            if (hasExtra(baseComponent)) {
-                baseComponent.setExtra(getReplacedExtra(baseComponent, user));
-            }
-            baseComponents[i] = baseComponent;
-        }
-
-        return baseComponents;
-    }
-
-    @Nonnull
-    private List<BaseComponent> getReplacedExtra(@Nonnull BaseComponent baseComponent, @Nonnull User user) {
-        Validate.notNull(baseComponent, "BaseComponent cannot be null");
-        LinkedList<BaseComponent> replacedExtra = new LinkedList<>();
-        for (BaseComponent extra : baseComponent.getExtra()) {
-            extra = getReplacedComponent(extra, user);
-            if (hasExtra(extra)) {
-                extra.setExtra(getReplacedExtra(extra, user));
-            }
-            replacedExtra.add(extra);
-        }
-        return replacedExtra;
-    }
-
-    @Nonnull
-    private BaseComponent getReplacedComponent(@Nonnull BaseComponent baseComponent, @Nonnull User user) {
-        Validate.notNull(baseComponent, "BaseComponent cannot be null");
-        if (baseComponent instanceof TextComponent) {
-            TextComponent textComponent = ((TextComponent) baseComponent);
-            textComponent.setText(ProtocolStringReplacer.getInstance().getReplacerManager().getReplacedString(((TextComponent) baseComponent).getText(), user, filter));
-        }
-        return baseComponent;
-    }
-
     private boolean isPaperComponent(Object object) {
         return ProtocolStringReplacer.getInstance().isPaper() && object instanceof net.kyori.adventure.text.TextComponent;
+    }
+
+    private GsonComponentSerializer getPaperGsonComponentSerializer() {
+        if (paperGsonComponentSerializer == null) {
+            paperGsonComponentSerializer = PaperComponents.gsonSerializer();
+        }
+        return paperGsonComponentSerializer;
     }
 
 }
