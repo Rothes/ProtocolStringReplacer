e9e4b15f8c00f23091160b3eef0466bb498f9972
Rothes <449181985@qq.com>
2021-07-04 18:53:19 +0800

    Updated v1.3.0.

--- src/main/java/me/Rothes/ProtocolStringReplacer/PacketListeners/Server/ItemStack/WindowItems_11.java
+++ src/main/java/me/Rothes/ProtocolStringReplacer/PacketListeners/Server/ItemStack/WindowItems_11.java
@@ -4,23 +4,20 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.BukkitConverters;
-import me.Rothes.ProtocolStringReplacer.ProtocolStringReplacer;
 import me.Rothes.ProtocolStringReplacer.User.User;
+import me.Rothes.ProtocolStringReplacer.ProtocolStringReplacer;
 import org.bukkit.inventory.ItemStack;
 
-public class WindowItems extends AbstractServerItemPacketListener {
+public final class WindowItems_11 extends AbstractServerItemPacketListener {
 
-    public WindowItems() {
+    public WindowItems_11() {
         super(PacketType.Play.Server.WINDOW_ITEMS);
     }
 
     public final PacketAdapter packetAdapter = new PacketAdapter(ProtocolStringReplacer.getInstance(), ListenerPriority.HIGHEST, packetType) {
         public void onPacketSending(PacketEvent packetEvent) {
             User user = getEventUser(packetEvent);
-            Object[] read = (Object[]) packetEvent.getPacket().getModifier().read(1);
-            for (var item : read) {
-                ItemStack itemStack = BukkitConverters.getItemStackConverter().getSpecific(item);
+            for (ItemStack itemStack : packetEvent.getPacket().getItemListModifier().read(0)) {
                 if (itemStack.hasItemMeta()) {
                     ItemStack original = itemStack.clone();
                     ProtocolStringReplacer.getInstance().getReplacerManager().getReplacedItemStack(itemStack, user, itemFilter);
