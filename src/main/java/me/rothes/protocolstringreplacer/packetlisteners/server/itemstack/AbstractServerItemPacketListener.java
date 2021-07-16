fe4aa0359047f3c564694f79bc3b98223e21286a
Rothes <449181985@qq.com>
2021-07-17 00:04:25 +0800

    Code Style.

--- src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/itemstack/AbstractServerItemPacketListener.java
+++ src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/itemstack/AbstractServerItemPacketListener.java
@@ -35,31 +35,29 @@ public abstract class AbstractServerItemPacketListener extends AbstractServerPac
     }
 
     protected void saveUserMetaCacche(User user, ItemStack originalItem, ItemStack replacedItem) {
-        if (user.hasPermission("protocolstringreplacer.feature.usermetacache")) {
-            if (originalItem.hasItemMeta()) {
-                ItemMeta originalMeta = originalItem.getItemMeta();
-                ItemMeta replacedMeta = replacedItem.getItemMeta();
-                if (!originalMeta.equals(replacedMeta)) {
-                    Short uniqueCacheKey = user.nextUniqueCacheKey();
-                    if (ProtocolStringReplacer.getInstance().getServerMajorVersion() >= 13) {
-                        CustomItemTagContainer tagContainer = replacedMeta.getCustomTagContainer();
-                        tagContainer.setCustomTag(getUserCacheKey(), ItemTagType.SHORT, uniqueCacheKey);
-                    } else {
-                        List<String> lore = replacedMeta.getLore();
-                        if (lore == null) {
-                            lore = new ArrayList<>();
-                        }
-                        StringBuilder stringBuilder = new StringBuilder();
-                        stringBuilder.append("§p§s§r§-§x");
-                        for (char Char : uniqueCacheKey.toString().toCharArray()) {
-                            stringBuilder.append('§').append(Char);
-                        }
-                        lore.add(stringBuilder.toString());
-                        replacedMeta.setLore(lore);
+        if (user.hasPermission("protocolstringreplacer.feature.usermetacache") && originalItem.hasItemMeta()) {
+            ItemMeta originalMeta = originalItem.getItemMeta();
+            ItemMeta replacedMeta = replacedItem.getItemMeta();
+            if (!originalMeta.equals(replacedMeta)) {
+                Short uniqueCacheKey = user.nextUniqueCacheKey();
+                if (ProtocolStringReplacer.getInstance().getServerMajorVersion() >= 13) {
+                    CustomItemTagContainer tagContainer = replacedMeta.getCustomTagContainer();
+                    tagContainer.setCustomTag(getUserCacheKey(), ItemTagType.SHORT, uniqueCacheKey);
+                } else {
+                    List<String> lore = replacedMeta.getLore();
+                    if (lore == null) {
+                        lore = new ArrayList<>();
                     }
-                    replacedItem.setItemMeta(replacedMeta);
-                    user.getMetaCache().put(uniqueCacheKey, originalMeta);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("§p§s§r§-§x");
+                    for (char Char : uniqueCacheKey.toString().toCharArray()) {
+                        stringBuilder.append('§').append(Char);
+                    }
+                    lore.add(stringBuilder.toString());
+                    replacedMeta.setLore(lore);
                 }
+                replacedItem.setItemMeta(replacedMeta);
+                user.getMetaCache().put(uniqueCacheKey, originalMeta);
             }
         }
     }
