f7dd2b816f9afc1cfe006e8e91f55cf115ec93ef
Rothes <449181985@qq.com>
2021-08-23 15:57:10 +0800

    Ignore an exception caused by ProtocolLib.

--- src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/EntityMetadata.java
+++ src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/EntityMetadata.java
@@ -12,6 +12,7 @@ import me.rothes.protocolstringreplacer.packetwrapper.WrapperPlayServerEntityMet
 import me.rothes.protocolstringreplacer.replacer.ListenType;
 import me.rothes.protocolstringreplacer.user.User;
 import net.md_5.bungee.chat.ComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
@@ -27,7 +28,15 @@ public final class EntityMetadata extends AbstractServerPacketListener {
 
     protected void process(PacketEvent packetEvent) {
         PacketContainer packet = packetEvent.getPacket();
-        Entity entity = packet.getEntityModifier(packetEvent).read(0);
+        Entity entity;
+        try {
+            entity = packet.getEntityModifier(packetEvent).read(0);
+        } catch (IllegalArgumentException exception) {
+            Bukkit.getConsoleSender().sendMessage("§7[§cProtocol§6StringReplacer§7] §c解析实体时发生异常. 这是 ProtocolLib 导致的错误, 请不要向 PSR 的作者反馈.");
+            Bukkit.getConsoleSender().sendMessage("§7[§cProtocol§6StringReplacer§7] §c此异常一般不会导致问题. 但您也可以自行构建下方的 ProtocolLib, 来避免此异常.");
+            Bukkit.getConsoleSender().sendMessage("§7[§cProtocol§6StringReplacer§7] §cGit 链接: https://github.com/Rothes/ProtocolLib.git");
+            return;
+        }
         if (entity == null || (!ProtocolStringReplacer.getInstance().getConfigManager().listenDroppedItemEntity && entity.getType() == EntityType.DROPPED_ITEM)) {
             return;
         }
