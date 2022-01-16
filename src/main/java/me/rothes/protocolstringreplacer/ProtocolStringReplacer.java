2995a3f6a21f6c0b90e5fe1d2b1925381733b73f
Rothes <449181985@qq.com>
2022-01-16 13:22:58 +0800

    Bump version -> 2.5.0

--- src/main/java/me/rothes/protocolstringreplacer/ProtocolStringReplacer.java
+++ src/main/java/me/rothes/protocolstringreplacer/ProtocolStringReplacer.java
@@ -49,7 +49,7 @@ import java.util.regex.Pattern;
 public class ProtocolStringReplacer extends JavaPlugin {
 
     public static final String VERSION_CHANNCEL = "Stable";
-    public static final int VERSION_NUMBER = 79;
+    public static final int VERSION_NUMBER = 80;
     private static ProtocolStringReplacer instance;
     private static Logger logger;
     private final HashMap<String, Integer> msgTimes = new HashMap<>();
