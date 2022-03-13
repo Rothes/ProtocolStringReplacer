103025b8e4b20f9caa40cd2a12a6e9e084b14cdc
Rothes <449181985@qq.com>
2022-03-13 16:08:47 +0800

    Improve

--- src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/Chat.java
+++ src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/Chat.java
@@ -9,14 +9,20 @@ import io.papermc.paper.text.PaperComponents;
 import me.rothes.protocolstringreplacer.ProtocolStringReplacer;
 import me.rothes.protocolstringreplacer.replacer.ListenType;
 import me.rothes.protocolstringreplacer.api.user.PsrUser;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 
+import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Optional;
 
 public final class Chat extends AbstractServerPacketListener {
 
+    private boolean setup = false;
+    private int bungeeComponentsField = -1;
+    private int paperComponentField = -1;
     private GsonComponentSerializer paperGsonComponentSerializer;
 
     public Chat() {
@@ -42,34 +48,55 @@ public final class Chat extends AbstractServerPacketListener {
                 }
 
             } else {
-                StructureModifier<Object> structureModifier = packet.getModifier();
-                for (int fieldIndex = 1; fieldIndex < 4 && fieldIndex < structureModifier.size(); fieldIndex++) {
-                    Object read = structureModifier.read(fieldIndex);
-                    if (read instanceof BaseComponent[]) {
-                        BaseComponent[] readComponents = (BaseComponent[]) read;
-                        json = ComponentSerializer.toString(readComponents);
+                StructureModifier<Object> modifier = packet.getModifier();
+                setupFields(modifier.getFields());
+                if (bungeeComponentsField != -1) {
+                    ProtocolStringReplacer.info(String.valueOf(modifier.getFields()));
+                    Object obj = modifier.read(bungeeComponentsField);
 
-                        WrappedChatComponent replaced = getReplacedJsonWrappedComponent(packetEvent, user, listenType, json, filter);
-                        if (replaced != null) {
-                            wrappedChatComponentStructureModifier.write(0, replaced);
-                            structureModifier.write(fieldIndex, null);
-                        }
-                    } else if (isPaperComponent(read)) {
-                        json = getPaperGsonComponentSerializer().serialize((net.kyori.adventure.text.Component) read);
+                    if (obj == null) {
+                        if (paperComponentField != -1) {
+                            obj = modifier.read(paperComponentField);
+                            Component component = (Component) obj;
+                            json = getPaperGsonComponentSerializer().serialize(component);
 
-                        WrappedChatComponent replaced = getReplacedJsonWrappedComponent(packetEvent, user, listenType, json, filter);
-                        if (replaced != null) {
-                            wrappedChatComponentStructureModifier.write(0, replaced);
-                            structureModifier.write(fieldIndex, null);
+                            WrappedChatComponent replaced = getReplacedJsonWrappedComponent(packetEvent, user, listenType, json, filter);
+                            if (replaced != null) {
+                                wrappedChatComponentStructureModifier.write(0, replaced);
+                                modifier.write(paperComponentField, null);
+                            }
                         }
+                        return;
+                    }
+
+                    BaseComponent[] components = (BaseComponent[]) obj;
+                    json = ComponentSerializer.toString(components);
+
+                    WrappedChatComponent replaced = getReplacedJsonWrappedComponent(packetEvent, user, listenType, json, filter);
+                    if (replaced != null) {
+                        wrappedChatComponentStructureModifier.write(0, replaced);
+                        modifier.write(bungeeComponentsField, null);
                     }
                 }
             }
         }
     }
 
-    private boolean isPaperComponent(Object object) {
-        return ProtocolStringReplacer.getInstance().hasPaperComponent() && object instanceof net.kyori.adventure.text.Component;
+    private void setupFields(List<Field> fields) {
+        if (setup) {
+            return;
+        }
+        for (int i = 0; i < fields.size(); i++) {
+            Field field = fields.get(i);
+            Class<?> type = field.getType();
+            if (ProtocolStringReplacer.getInstance().isSpigot() && type.getCanonicalName().equals("net.md_5.bungee.api.chat.BaseComponent[]")) {
+                ProtocolStringReplacer.info(field +".");
+                bungeeComponentsField = i;
+            } else if (ProtocolStringReplacer.getInstance().hasPaperComponent() && type == Component.class) {
+                paperComponentField = i;
+            }
+        }
+        setup = true;
     }
 
     private GsonComponentSerializer getPaperGsonComponentSerializer() {
