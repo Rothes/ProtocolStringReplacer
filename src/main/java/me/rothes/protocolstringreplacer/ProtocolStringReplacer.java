19953145b1bfa50579e8267d0f2d287367dd268d
Rothes <449181985@qq.com>
2022-05-21 13:48:10 +0800

    Bump plugin version -> '2.7.0'

--- src/main/java/me/rothes/protocolstringreplacer/ProtocolStringReplacer.java
+++ src/main/java/me/rothes/protocolstringreplacer/ProtocolStringReplacer.java
@@ -49,7 +49,7 @@ import java.util.regex.Pattern;
 public class ProtocolStringReplacer extends JavaPlugin {
 
     public static final String VERSION_CHANNCEL = "Stable";
-    public static final int VERSION_NUMBER = 89;
+    public static final int VERSION_NUMBER = 90;
     private static ProtocolStringReplacer instance;
     private static Logger logger;
     private final HashMap<String, Integer> msgTimes = new HashMap<>();
