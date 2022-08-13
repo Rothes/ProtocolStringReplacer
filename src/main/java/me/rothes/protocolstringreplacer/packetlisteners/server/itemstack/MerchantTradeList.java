697832998ea8e02e8bdba6caed57633d9e51bb17
Rothes <449181985@qq.com>
2022-08-14 02:35:37 +0800

    Fix `OPEN_WINDOW_MERCHANT` handling

--- src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/itemstack/MerchantTradeList.java
+++ src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/itemstack/MerchantTradeList.java
@@ -1,12 +1,17 @@
 package me.rothes.protocolstringreplacer.packetlisteners.server.itemstack;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
+import me.rothes.protocolstringreplacer.ProtocolStringReplacer;
+import me.rothes.protocolstringreplacer.api.replacer.ReplacerConfig;
 import me.rothes.protocolstringreplacer.api.user.PsrUser;
+import me.rothes.protocolstringreplacer.replacer.ReplacerManager;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MerchantTradeList extends AbstractServerItemPacketListener {
@@ -20,15 +25,30 @@ public class MerchantTradeList extends AbstractServerItemPacketListener {
         if (user == null) {
             return;
         }
-        StructureModifier<List<MerchantRecipe>> merchantRecipeLists = packetEvent.getPacket().getMerchantRecipeLists();
+        PacketContainer packet = packetEvent.getPacket().deepClone();
+        StructureModifier<List<MerchantRecipe>> merchantRecipeLists = packet.getMerchantRecipeLists();
 
+        ReplacerManager replacerManager = ProtocolStringReplacer.getInstance().getReplacerManager();
+        List<ReplacerConfig> replacers = replacerManager.getAcceptedReplacers(user, itemFilter);
+
+        List<MerchantRecipe> replaced = new ArrayList<>();
+
+        MerchantRecipe toAdd;
         List<MerchantRecipe> read = merchantRecipeLists.read(0);
         for (MerchantRecipe recipe : read) {
-            for (ItemStack ingredient : recipe.getIngredients()) {
-                replaceItemStack(packetEvent, user, listenType, ingredient, filter);
+            List<ItemStack> ingredients = recipe.getIngredients();
+            for (ItemStack ingredient : ingredients) {
+                replaceItemStack(packetEvent, user, listenType, ingredient, replacers, false);
             }
+            replaceItemStack(packetEvent, user, listenType, recipe.getResult(), replacers, false);
 
-            replaceItemStack(packetEvent, user, listenType, recipe.getResult(), filter);
+            toAdd = new MerchantRecipe(recipe.getResult(), recipe.getUses(), recipe.getMaxUses(),
+                    recipe.hasExperienceReward(), recipe.getVillagerExperience(), recipe.getPriceMultiplier(),
+                    recipe.getDemand(), recipe.getSpecialPrice());
+            toAdd.setIngredients(ingredients);
+            replaced.add(toAdd);
         }
+        merchantRecipeLists.write(0, replaced);
+        packetEvent.setPacket(packet);
     }
 }
