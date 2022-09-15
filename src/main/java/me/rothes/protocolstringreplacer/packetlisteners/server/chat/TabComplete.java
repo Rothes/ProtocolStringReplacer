df1fbc2bbd23d8d62ecc96a28771163b5a00eac8
Rothes <449181985@qq.com>
2022-09-15 18:13:00 +0800

    Improve `TAB_COMPLETE` handling on 1.13+

--- src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/chat/TabComplete.java
+++ src/main/java/me/rothes/protocolstringreplacer/packetlisteners/server/chat/TabComplete.java
@@ -17,8 +17,6 @@ import java.util.List;
 
 public class TabComplete extends AbstractServerPacketListener {
 
-    private int suggestionsField = -1;
-
     public TabComplete() {
         super(PacketType.Play.Server.TAB_COMPLETE, ListenType.TAB_COMPLETE);
     }
@@ -31,24 +29,10 @@ public class TabComplete extends AbstractServerPacketListener {
             return;
         }
 
-        // 1.13+
-        StructureModifier<Object> modifier = packet.getModifier();
-        if (suggestionsField == -1) {
-            Object read;
-            try {
-                for (int i = 0; i < modifier.size(); i++) {
-                    read = modifier.read(i);
-                    if (read instanceof Suggestions) {
-                        suggestionsField = i;
-                        break;
-                    }
-                }
-            } catch (NoClassDefFoundError ignored) {
-                suggestionsField = -2;
-            }
-        }
-        if (suggestionsField != -2) {
-            Suggestions suggestions = (Suggestions) modifier.read(suggestionsField);
+        if (ProtocolStringReplacer.getInstance().getServerMajorVersion() >= 13) {
+            // 1.13+
+            StructureModifier<Suggestions> modifier = packet.getModifier().withType(Suggestions.class);
+            Suggestions suggestions = modifier.read(0);
 
             List<Suggestion> list = suggestions.getList();
             Suggestion suggestion;
@@ -58,15 +42,14 @@ public class TabComplete extends AbstractServerPacketListener {
                         getReplacedText(packetEvent, user, listenType, suggestion.getText(), filter),
                         suggestion.getTooltip()));
             }
-            return;
-        }
-
-        // 1.8 - 1.12
-        StructureModifier<String[]> stringArrays = packet.getStringArrays();
-        if (stringArrays.size() != 0) {
-            String[] read = stringArrays.read(0);
-            for (int i = 0; i < read.length; i++) {
-                read[i] = getReplacedText(packetEvent, user, listenType, read[i], filter);
+        } else {
+            // 1.8 - 1.12
+            StructureModifier<String[]> stringArrays = packet.getStringArrays();
+            if (stringArrays.size() != 0) {
+                String[] read = stringArrays.read(0);
+                for (int i = 0; i < read.length; i++) {
+                    read[i] = getReplacedText(packetEvent, user, listenType, read[i], filter);
+                }
             }
         }
 
