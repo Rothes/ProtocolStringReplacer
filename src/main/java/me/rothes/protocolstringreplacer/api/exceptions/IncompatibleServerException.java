c98dc416dd1e2f31917a0a1dbcc9fc1a5885c905
Rothes <449181985@qq.com>
2023-02-15 21:18:05 +0800

    fixup! Throw IncompatibleServerException instead of RuntimeException

---
+++ src/main/java/me/rothes/protocolstringreplacer/api/exceptions/IncompatibleServerException.java
@@ -0,0 +1,9 @@
+package me.rothes.protocolstringreplacer.api.exceptions;
+
+public class IncompatibleServerException extends RuntimeException {
+
+    public IncompatibleServerException(Throwable throwable) {
+        super(throwable);
+    }
+
+}
