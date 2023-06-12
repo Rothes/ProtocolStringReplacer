8d291873c5822694781caaf2f2e49ca86ed78067
Rothes <449181985@qq.com>
2023-06-12 19:40:17 +0800

    Bump plugin version -> `2.18.1`

--- src/main/java/me/rothes/protocolstringreplacer/Updater.java
+++ src/main/java/me/rothes/protocolstringreplacer/Updater.java
@@ -28,7 +28,7 @@ import java.util.Map;
 public class Updater implements Listener {
 
     private static final String VERSION_CHANNEL = "Stable";
-    private static final int VERSION_NUMBER = 112;
+    private static final int VERSION_NUMBER = 113;
     private final HashMap<String, Integer> msgTimesMap = new HashMap<>();
     private final List<String> messages = new ArrayList<>();
     private final ProtocolStringReplacer plugin;
