88c851b3753283bb20127bc9a008334d3c5de0c1
Rothes <449181985@qq.com>
2024-04-22 10:13:03 +0800

    删库跑路(

--- bukkit/src/main/java/io/github/rothes/protocolstringreplacer/bukkit/api/exceptions/MissingInitialResourceException.java
+++
@@ -1,9 +0,0 @@
-package io.github.rothes.protocolstringreplacer.bukkit.api.exceptions;
-
-public class MissingInitialResourceException extends RuntimeException {
-
-    public MissingInitialResourceException(String message) {
-        super(message);
-    }
-
-}
--- bukkit/src/main/java/io/github/rothes/protocolstringreplacer/bukkit/replacer/containers/Replaceable.java
+++
@@ -1,9 +0,0 @@
-package io.github.rothes.protocolstringreplacer.bukkit.replacer.containers;
-
-public interface Replaceable {
-
-    String getText();
-
-    void setText(String text);
-
-}
