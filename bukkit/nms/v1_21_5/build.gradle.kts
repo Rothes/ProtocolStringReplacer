f7a15be419aafff9b60ada7458305b517dc1201a
Rothes <449181985@qq.com>
2025-06-19 12:34:13 +0800

    To moonrise dev-bundle

--- bukkit/nms/v1_21_5/build.gradle.kts
+++ bukkit/nms/v1_21_5/build.gradle.kts
@@ -1,6 +1,6 @@
 val serverVer = "1.21.5"
 
 dependencies {
-    paperweight.paperDevBundle("$serverVer-no-moonrise-SNAPSHOT")
+    paperweight.paperDevBundle("$serverVer-R0.1-SNAPSHOT")
     implementation(project(":bukkit:nms:common:generic_v1_21_3"))
 }
