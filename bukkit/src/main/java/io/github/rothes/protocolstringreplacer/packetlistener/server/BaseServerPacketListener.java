d6f32771c9fcafefa5152de2d34e42dea74771c8
Rothes <449181985@qq.com>
2025-08-04 16:32:37 +0800

    Remove unused import

--- bukkit/src/main/java/io/github/rothes/protocolstringreplacer/packetlistener/server/BaseServerPacketListener.java
+++ bukkit/src/main/java/io/github/rothes/protocolstringreplacer/packetlistener/server/BaseServerPacketListener.java
@@ -27,7 +27,6 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
