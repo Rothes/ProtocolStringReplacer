02e27207f312c6e9615d806d80e0db595dabccbc
Rothes <449181985@qq.com>
2025-09-15 22:23:31 +0800

    Support Item hover on 1.20.5+

---
+++ bukkit/src/main/kotlin/io/github/rothes/protocolstringreplacer/replacer/containers/ItemContentContainer.kt
@@ -0,0 +1,52 @@
+@file:Suppress("DEPRECATION")
+
+package io.github.rothes.protocolstringreplacer.replacer.containers
+
+import de.tr7zw.nbtapi.NBT
+import io.github.rothes.protocolstringreplacer.plugin
+import net.md_5.bungee.api.chat.ItemTag
+import net.md_5.bungee.api.chat.hover.content.Item
+
+open class ItemContentContainer(item: Item, root: Container<*>?) : AbstractContainer<Item>(item, root) {
+
+    private val container: ItemStackContainer?
+
+    init {
+        if (content.tag != null) {
+            val nbt = NBT.createNBTObject()
+            nbt.setString("id", content.id)
+            if (NEW_NBT) {
+                nbt.setInteger("count", content.count)
+                nbt.getOrCreateCompound("components").mergeCompound(NBT.parseNBT(content.tag.nbt))
+            } else {
+                nbt.setInteger("Count", content.count)
+                nbt.getOrCreateCompound("tag").mergeCompound(NBT.parseNBT(content.tag.nbt))
+            }
+            val itemStack = NBT.itemStackFromNBT(nbt)!!
+            container = ItemStackContainer(itemStack, false, root)
+        } else {
+            container = null
+        }
+    }
+
+    override fun createDefaultChildren() {
+        if (container != null) {
+            children.add(container)
+            container.entriesPeriod()
+        }
+    }
+
+    override fun getResult(): Item {
+        if (container != null) {
+            val itemStack = container.result
+            val nbt = NBT.itemStackToNBT(itemStack)
+            content.tag = ItemTag.ofNbt(if (NEW_NBT) nbt.getCompound("components").toString() else nbt.getCompound("tag").toString())
+        }
+        return content
+    }
+
+    companion object {
+        private val NEW_NBT = plugin.serverMajorVersion == 20.toByte() && plugin.serverMinorVersion >= 5 || plugin.serverMajorVersion > 20
+    }
+
+}
